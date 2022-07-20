26edbbe4b7b6aa12ccbc5c84035032d0af5abc36
Swapnilk98 <99746007+Swapnilk98@users.noreply.github.com>
2022-07-20 10:39:41 +0530

    Update Navbar.jsx
    h

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
+=======
+>>>>>>> Stashed changes
 const Navbar = () => {
   return <div>Navbar</div>;
 };
 
 export default Navbar;
+<<<<<<< Updated upstream
 =======
 export const Navbar = () => {
   return <div>Navbar</div>;
 };
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
