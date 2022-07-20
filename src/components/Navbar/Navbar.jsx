f90a0572f0ff68d224155fbbaae9975269943b65
Swapnilk98 <99746007+Swapnilk98@users.noreply.github.com>
2022-07-20 09:28:45 +0530

    Update Navbar.jsx
    check

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
+<<<<<<< Updated upstream
 const Navbar = () => {
   return <div>Navbar</div>;
 };
 
 export default Navbar;
+=======
+export const Navbar = () => {
+  return <div>Navbar</div>;
+};
+>>>>>>> Stashed changes
