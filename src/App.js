94942027214c8a87aca443a4507a34ecb6b589b7
moazzemhosen <92745004+moazzemhosen@users.noreply.github.com>
2022-07-22 23:20:45 +0530

    day2
    day2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { LargeWithLogoLeft } from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
+import Suggestion from "./components/Suggestion/Suggestion";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Suggestion/>
       <LargeWithLogoLeft />
     </div>
   );
---
+++ src/components/Suggestion/Suggestion.jsx
@@ -0,0 +1,66 @@
+import "./Suggestion.css";
+const Suggestion = () => {
+  return (
+    <div>
+      <div className="head">
+        <p> Goa Tourism & Travel Guide</p>
+        <b>Goa Tourism and Travel Guide</b>
+      </div>
+      <div className="para">
+        <p>
+          Famous for its beaches, Goa is the smallest state in India in terms of
+          area, the fourth smallest state in terms of the population yet a huge
+          contributor to the economy through Goa tourism. Panaji is the capital
+          of Goa. Goa Tourism is considered to be the major support to Goa’s
+          economy. Through Tripoto’s Goa tourism and travel guide you can
+          explore the best of Goa. Thinking of ‘where to go in Goa’? Check out
+          the below Goa tour guide and get all the necessary information about
+          Goa tourism. When in India’s sunshine state, be prepared to fall in
+          love with sandy beaches, palm-fringed shores and beachfront Portuguese
+          bungalows. Goa will never fail to entice you with its landscapes
+          peppered with paddy fields, coconut groves, windy cliffs and riverside
+          forts. While the southern part of the state is a haven for tourists
+          seeking quiet days spent by the tranquil seaside, Goa tourism in the
+          north is known primarily because of its bustling shacks and legendary
+          nightlife. From the umpteen number of places to visit in Goa, a few
+          that most definitely shouldn’t be missed are the Fontainhas – India’s
+          only existing Latin quarters, the 17th century old Bom Jesus Basilica;
+          to revel in a little bohemian spirit, the vibrant Wednesday flea
+          market at Anjuna Beach, and Dudhsagar – one of India’s highest
+          waterfalls. But if you’re here simply to unwind, there’s no dearth of
+          things to enjoy. Starting with spotting a dolphin or two at the
+          Palolem beach, marveling at the routinely glorious sunsets at Chapora
+          fort, taking leisurely strolls through the mossy ruins of St.
+          Augustine in Old Goa or simply soaking in the salty tropical breeze
+          with platefuls of butter garlic prawns, bebinca and a few too many rum
+          and cokes, with a side of jazz and Konkani music. Wondering how much a
+          visit to Goa will cost you? Goa tourism has made it easier for all
+          kinds of travellers with a daily budget between Rs.3000 to Rs.5000. If
+          your budget is tighter, read on to see what Goa tourism offers to its
+          budget travellers and also for a fair breakdown of your expenses in
+          Goa.
+        </p>
+      </div>
+      <div className="btn">
+        <button>Get Customised Packages</button>
+      </div>
+      <div className="btns">
+        <button>Overview</button>
+        <button>Trips</button>
+        <button>Places</button>
+        <button>Hotels</button>
+      </div>
+      <div className="head2">
+        <h1>All You Need To Know About Goa Tourism</h1>
+      </div>
+      <div className="para">
+        <p>
+          Read real accounts of travellers in Goa, upcoming events, latest news,
+          and find all you need to know to discover India's favourite beach
+          town!
+        </p>
+      </div>
+    </div>
+  );
+};
+export default Suggestion;
