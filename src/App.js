1ae79a45cac0791fbe7fc9696bd3a620dc7fe9e0
moazzemhosen <92745004+moazzemhosen@users.noreply.github.com>
2022-07-22 23:37:01 +0530

    SuggetionUpdate
    SuggetionUpdate

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import { LargeWithLogoLeft } from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
-import Suggestion from "./components/Suggestion/Suggestion";
+
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Suggestion/>
       <LargeWithLogoLeft />
     </div>
   );
