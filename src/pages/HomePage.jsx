b22d39467afc48fc41748644722bd354fce0168c
moazzemhosen <92745004+moazzemhosen@users.noreply.github.com>
2022-07-22 20:48:24 +0530

    Create HomePage.jsx
    done

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,30 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+import Animation from "../components/HomePage/Animation";
+import CkeckOutHotels from "../components/HomePage/CkeckOutHotels";
+import FindPlaces from "../components/HomePage/FindPlaces";
+import MiniCard from "../components/card/MiniCard";
+import Partners from "../components/HomePage/Partners";
+import PlanTrip from "../components/HomePage/PlanTrip";
+import SearchbarArea from "../components/SearchbarArea/SearchbarArea";
+import TravelLearn from "../components/HomePage/TravelLearn";
+
+const HomePage = () => {
+  return (
+    <div>
+      <SearchbarArea />
+      <Box w={"80%"} m={"auto"} border={"1px solid red"}>
+        <MiniCard />
+        <hr />
+        <Partners />
+        <Animation />
+        <FindPlaces />
+        <PlanTrip />
+        <CkeckOutHotels />
+        <TravelLearn />
+      </Box>
+    </div>
+  );
+};
+
+export default HomePage;
