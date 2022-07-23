abc79c1e91675112cec83b7bb10f9e799f0ab52d
Swapnilk98 <99746007+Swapnilk98@users.noreply.github.com>
2022-07-23 14:20:49 +0530

    bottom part
    done

--- src/App.js
+++ src/App.js
@@ -1,38 +1,16 @@
-<<<<<<< Updated upstream
-import logo from './logo.svg';
-import './App.css';
-=======
 //import { LargeWithLogoLeft } from "./components/Footer/Footer";
 //import Navbar from "./components/Navbar/Navbar";
 
 import Bottom from "./components/Suggestion/Bottom";
 //import Suggestion from "./components/Suggestion/Suggestion";
->>>>>>> Stashed changes
 
 function App() {
   return (
     <div className="App">
-<<<<<<< Updated upstream
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-=======
       {/* <Navbar />
       <LargeWithLogoLeft />
       <Suggestion /> */}
       <Bottom />
->>>>>>> Stashed changes
     </div>
   );
 }
--- src/components/Suggestion/Bottom.jsx
+++ src/components/Suggestion/Bottom.jsx
@@ -4,7 +4,7 @@ const Bottom = () => {
   return (
     <div>
       <div className="head2">
-        <h1>Budget for Goa</h1>
+        <b>Budget for Goa</b>
       </div>
       <div className="para2">
         <b>For Budget Travellers: INR 600 - 1200 a day</b>
@@ -19,22 +19,142 @@ const Bottom = () => {
             <li>Motorbike rentals: INR 200 - 400</li>
             <li>Sightseeing (museums etc): INR 10 - 50</li>
           </ul>
-          <div className="para">
-            <b>For Mid-range Travellers: INR 1000 to INR 3000 a day</b>
-          </div>
-          <div className="bullet">
-            <ul>
-              <li>
-                Accommodation in mid-range hotels and resorts: INR 600 - 2000
-              </li>
-              <li>Food in mid-range cafes and restaurants: INR 400 - 700</li>
-              <li>
-                Local transport in local taxis or car rentals: INR 400 - 600
-              </li>
-              <li>Snorkelling and Diving packages: INR 1500 and upwards</li>
-            </ul>
-          </div>
         </div>
+      </div>
+      <div className="para2">
+        <b>For Mid-range Travellers: INR 1000 to INR 3000 a day</b>
+
+        <div className="bullet">
+          <ul>
+            <li>
+              Accommodation in mid-range hotels and resorts: INR 600 - 2000
+            </li>
+            <li>Food in mid-range cafes and restaurants: INR 400 - 700</li>
+            <li>
+              Local transport in local taxis or car rentals: INR 400 - 600
+            </li>
+            <li>Snorkelling and Diving packages: INR 1500 and upwards</li>
+          </ul>
+        </div>
+      </div>
+      <div className="para2">
+        <b>For Luxury Travellers: INR 6000 and upwards</b>
+
+        <div className="bullet">
+          <ul>
+            <li>Accommodation in 4* or 5* hotels: INR 4000 and upwards</li>
+            <li>Food in upscale restaurants and bars: INR 1000 and upwards</li>
+            <li>Local transport in cabs: INR 400 and upwards</li>
+            <li>
+              Sightseeing of Goa tourism, Guided Tours, Kayaking: INR 1000 and
+              upwards
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="head2">
+        <b>Top Attractions in Goa</b>
+      </div>
+      <div className="para3">
+        <p>
+          The land of sand, surf, and sunshine has been a favourite among
+          travellers from around the world for quite some time. There is nowhere
+          in India quite like it. Goa tourism is the primary source of income
+          for residents of Goa. Not just domestic tourists, Goa is visited by
+          tourists from all over the world. Since Goa tourism focuses on its
+          pristine beaches, local shacks and delicious cuisine, you’ll find lots
+          of things to keep you busy in Goa. Besides the beaches, you can
+          explore the city’s churches, marvellous forts and museums which
+          equally contribute to the growing popularity of Goa tourism. Good food
+          is another thing that majorly contributed to Goa tourism. The blend of
+          Indian and Portuguese influences is reflected in the architectural
+          marvels of Goa tourism.
+        </p>
+      </div>
+      <div className="para4">
+        <p>
+          North Goa is the most famous area of Goa tourism. Known for its
+          energetic nightlife, Goan delicacies, flea markets, crowded beaches,
+          and shacks, North Goa tops the list. Apart from the enthusiastic crowd
+          and pretty shacks, North Goa beaches are also famous for different
+          water sports like parasailing, jet skiing, and banana boat rides.
+          North Goa is the happy place of Goa as it offers you a variety of
+          things to do and a lot of different places to see apart from just
+          lazing around the beaches. It offers something for everyone and has
+          been the driving force for Goa tourism.
+        </p>
+      </div>
+      <div className="para4">
+        <p>
+          South Goa is the other half of Goa that offers a tranquil atmosphere
+          to its tourists. It is filled with appealing beaches, less crowded
+          than the beaches in North Goa. It is a perfect area for a relaxing
+          vacation. Though South Goa tourism doesn’t offer as much as the places
+          in North Goa do, its calmness and peaceful vibes will make up for
+          everything. South Goa beaches are much whiter and cleaner than the
+          beaches in North Goa and Goa tourism has gradually picked up here.
+          Cotigao wildlife sanctuary, Cabo de Rama Fort and Dudhsagar falls are
+          some of the must-visit places in South Goa.
+        </p>
+      </div>
+      <div className="para4">
+        <p>
+          Cola Beach, Baga Beach, Calangute Beach, Betul Beach, Arambol Beach,
+          Basilica of Bom Jesus, Vagator Beach, Fort Aguada, Dudhsagar Falls,
+          Church Square are some of the recommendations by Goa tourism. Read
+          this article for more info on North Goa vs South Goa.
+        </p>
+      </div>
+      <div className="head2">
+        <b>Offbeat Places in Goa</b>
+      </div>
+      <div className="para3">
+        <p>
+          Apart from the stunning beaches, there are a lot of things to see in
+          Goa tourism such as its heritage, sightseeing attractions, nature &
+          parks, dolphin watching tours, architecture & museums, wildlife & bird
+          sanctuaries. These are the major tourist attractions in Goa and the
+          topmost contributors to Goa tourism. Opt for a HOHO tour for a
+          comfortable and enjoyable sightseeing experience of Goa tourism. Make
+          sure you visit the beautiful Chorla Ghats located at the border of
+          Goa, filled with Tropical Forests. The ‘hill station’ feel will
+          definitely make you forget the beaches. Also, explore Querim beach,
+          Bird Sanctuary, Harvalem Falls, Naval Aviation Museum, Museum of Goa,
+          Arvalem Caves, and Our Lady of the Mount are some of the
+          recommendations by Goa tourism.
+        </p>
+      </div>
+      <div className="head2">
+        <b>Where to Do Water Activities in Goa</b>
+      </div>
+      <div className="para3">
+        <p>
+          One of the biggest attractions for Goa tourism is the plethora of
+          water sports offered in the state. The most popular beach in Goa for
+          water sports are - Calangute Beach, Candolim Beach, Baga Beach, and
+          Sinquerim Beach.
+        </p>
+      </div>
+      <div className="para4">
+        <p>
+          Beaches in Goa are the major tourist attractions of Goa tourism and
+          the most beautiful beaches in India. Not just the beauty, they top the
+          list in terms of other things like thrilling water sports, relaxing
+          beach shacks, quality time with friends and family and exotic food.
+          Another good thing about these beaches is, they offer you something
+          new every time you visit Goa.
+        </p>
+      </div>
+      <div className="para4">
+        <p>
+          The most popular beaches in Goa are Baga, Anjuna, Calangute and
+          Candolim. Since Goa tourism is more developed for mainstream tourists
+          in North Goa, the beaches here are more crowded. You can opt for
+          Parasailing at Calangute Beach, Banana Boat Ride at Baga Beach, skiing
+          at Calangute Beach, Snorkeling & Scuba Diving at Grande Island,
+          Kayaking at Aguada River, Flyboarding at Vagator Beach and Speed
+          Boating at Calangute Beach.
+        </p>
       </div>
     </div>
   );
