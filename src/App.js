885613665b22b2668dc5ad6acffa0a07b203713c
moazzemhosen <92745004+moazzemhosen@users.noreply.github.com>
2022-07-23 15:48:36 +0530

    Update App.js
    update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
 
 import './App.css';
-// import Suggestion from './components/Suggestion/Suggestion';
-// import Navbar from './components/Navbar/Navbar';
- import HomePage from './pages/HomePage';
-// import { LargeWithLogoLeft } from "./components/Footer/Footer";
-
-
 function App() {
   return (
     <div className="App">
-     {/* <Navbar/>  */}
-       <HomePage/>
-      {/* <LargeWithLogoLeft />  */}
-       {/* <Suggestion/>  */}
-     
-
+   
     </div>
   );
 }
